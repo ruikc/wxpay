7b3a9559afec405bcf0f4ea5be4eed3440e22d92
于志远 <lhyfe1987@163.com>
2016-07-18 16:25:46 +0800

    init project

---
+++ WxPay.Exception.php
@@ -0,0 +1,14 @@
+<?php
+namespace y284663247\wxpay;
+/**
+ * 
+ * 微信支付API异常类
+ * @author widyhu
+ *
+ */
+class WxPayException extends \Exception {
+	public function errorMessage()
+	{
+		return $this->getMessage();
+	}
+}
